3c6c1a808a8098f396f119426bca7790748a35f5
Edgar.M <146377700+Toxi92@users.noreply.github.com>
2025-05-15 13:31:39 +0200

    CONTROLE EMAIL INSCRIPTION VIA REGEX

--- controlleur/controlleur_inscription.php
+++ controlleur/controlleur_inscription.php
@@ -5,6 +5,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    if (preg_match('/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/im', $email)==false) {
+        echo "<script>alert('Format d'adresse mail non valide.');</script>";
+        exit;
+    } 
     if ($query->isMailUsed($email)) {
         echo "<script>alert('L\'adresse e-mail est déjà utilisée.');</script>";
         exit;
